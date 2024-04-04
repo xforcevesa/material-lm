993630b92b3ee0b1fefa9061c2b84b4381cbf3ff
nomodeset <nomodeset@qq.com>
2024-04-04 19:22:41 +0800

    script

--- workspace/wget-lists/download.sh
+++ workspace/wget-lists/download.sh
@@ -1,6 +1,7 @@
+# shellcheck disable=SC2045
 for dir in $(ls)
 do
-    cd $dir
-    cat wget-list.txt | parallel --gnu "wget {}"
-    cd -
+    cd "$dir" || exit
+    wget -N -i wget-list.txt
+    cd - || exit
 done
